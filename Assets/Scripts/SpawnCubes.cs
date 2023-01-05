9c5f2c873473889ccfed6cd375c5d6f5bb0a5e58
gladvalaks <kotandotan@gmail.com>
2023-01-05 20:42:52 +0300

    Добавил возможность спавна персонажа

--- Assets/Scripts/SpawnCubes.cs
+++ Assets/Scripts/SpawnCubes.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class SpawnCubes : MonoBehaviour
 {
     [TextArea]
     public string TextCubesLocation;
     public GameObject cubePrefab;
-    
+    public GameObject playerPrefab;
+
     void Start()
     {
-        List<string> cubes = new List<string>(TextCubesLocation.Split());
-        SpawnObjects(cubes);
+       
+        SpawnObjects();
     }
 
     // Update is called once per frame
@@ -19,8 +21,10 @@ public class SpawnCubes : MonoBehaviour
     {
         
     }
-    void SpawnObjects(List<string> cubes)
+    [ContextMenu("Spawn Objects")]
+    void SpawnObjects()
     {
+        List<string> cubes = new List<string>(TextCubesLocation.Split());
         float basePosX = cubes[0].Length / 2f;
         float basePosY = cubes.Count / 2f;
         for (int i = 0; i < cubes.Count; i++)
@@ -28,6 +32,7 @@ public class SpawnCubes : MonoBehaviour
             for (int j = 0; j < cubes[0].Length; j++)
             {
                 GameObject cube = Instantiate(cubePrefab);
+                Undo.RegisterCreatedObjectUndo(cube, "Object");
                 cube.transform.position = new Vector3(-basePosX + j, 0, basePosY - i);
             }
         }
@@ -35,10 +40,21 @@ public class SpawnCubes : MonoBehaviour
         {
             for (int j = 0; j < cubes[0].Length; j++)
             {
-                if(int.Parse(cubes[i][j].ToString()) == 1)
+                int indexOfGameObject = int.Parse(cubes[i][j].ToString());
+                GameObject obj = null;
+                if (indexOfGameObject == 1)
                 {
-                    GameObject cube = Instantiate(cubePrefab);
-                    cube.transform.position = new Vector3(-basePosX + j, 1, basePosY - i);
+                    obj = Instantiate(cubePrefab);
+                    
+                }
+                else if(indexOfGameObject == 2)
+                {
+                    obj = Instantiate(playerPrefab);
+                }
+                if (indexOfGameObject != 0)
+                {
+                    Undo.RegisterCreatedObjectUndo(obj,"Object");
+                    obj.transform.position = new Vector3(-basePosX + j, 1, basePosY - i);
                 }
             }
         }
