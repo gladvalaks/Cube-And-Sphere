92c8dd20bb0024b592bcb6a2701fc4d151ae1dc9
gladvalaks <kotandotan@gmail.com>
2023-01-05 04:58:21 +0300

    Создал спавн кубов, он еще неготовый

--- Assets/Scripts/SpawnCubes.cs
+++ Assets/Scripts/SpawnCubes.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class SpawnCubes : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [TextArea]
+    public string TextCubesLocation;
+    public GameObject cubePrefab;
+    
     void Start()
     {
-        
+        List<string> cubes = new List<string>(TextCubesLocation.Split());
+        SpawnObjects(cubes);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    void SpawnObjects(List<string> cubes)
+    {
+        float basePosX = cubes[0].Length / 2f;
+        float basePosY = cubes.Count / 2f;
+        for (int i = 0; i < cubes.Count; i++)
+        {
+            for (int j = 0; j < cubes[0].Length; j++)
+            {
+                GameObject cube = Instantiate(cubePrefab);
+                cube.transform.position = new Vector3(-basePosX + j, 0, basePosY - i);
+            }
+        }
+        for (int i = 0; i < cubes.Count; i++)
+        {
+            for (int j = 0; j < cubes[0].Length; j++)
+            {
+                if(int.Parse(cubes[i][j].ToString()) == 1)
+                {
+                    GameObject cube = Instantiate(cubePrefab);
+                    cube.transform.position = new Vector3(-basePosX + j, 1, basePosY - i);
+                }
+            }
+        }
         
     }
 }
