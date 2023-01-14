a5e715291eb2f4555bae5c28541e74bf05c193e0
gladvalaks <kotandotan@gmail.com>
2023-01-14 18:52:05 +0300

    Добавил скрипт камеры, который динамически изменяет её размер
    убрал файлы

--- Assets/Scripts/SpawnCubes.cs
+++ Assets/Scripts/SpawnCubes.cs
@@ -12,6 +12,7 @@ public class SpawnCubes : MonoBehaviour
     public string TextCubesLocation;
     public CubeManager cubeManager;
     public LevelsScript levelsScript;
+    public CameraScript cameraScript;
     public GameObject cubePrefab;
     public GameObject playerPrefab;
     public Color color;
@@ -19,6 +20,7 @@ public class SpawnCubes : MonoBehaviour
     private void Awake()
     {
         levelsScript = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelsScript>();
+        cameraScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraScript>();
     }
     void Start()
     {
@@ -39,16 +41,7 @@ public class SpawnCubes : MonoBehaviour
         List<string> cubes = new List<string>(TextCubesLocation.Split());
         float basePosX = cubes[0].Length / 2f;
         float basePosY = cubes.Count / 2f;
-        for (int i = 0; i < cubes.Count; i++)
-        {
-            for (int j = 0; j < cubes[0].Length; j++)
-            {
-                GameObject cube = Instantiate(cubePrefab);
-                Undo.RegisterCreatedObjectUndo(cube, "Object");
-                cube.transform.position = new Vector3(-basePosX + j, 0, basePosY - i);
-                
-            }
-        }
+        cameraScript.SetCameraSize(basePosX * 2);
         for (int i = 0; i < cubes.Count; i++)
         {
             for (int j = 0; j < cubes[0].Length; j++)
@@ -57,27 +50,32 @@ public class SpawnCubes : MonoBehaviour
                 GameObject obj = null;
                 if (indexOfGameObject == 1)
                 {
-                    obj = Instantiate(cubePrefab);
+                    obj = Instantiate(cubePrefab, this.gameObject.transform);
                     obj.GetComponent<Renderer>().material.color = color;
 
                 }
                 else if(indexOfGameObject == 2)
                 {
-                    obj = Instantiate(playerPrefab);
+                    obj = Instantiate(playerPrefab, this.gameObject.transform);
                     cubeManager.cubesCount += 1;
+                    GameObject cube = Instantiate(cubePrefab,this.gameObject.transform);
+                    cube.transform.position = new Vector3(-basePosX + j, 0, basePosY - i);
                 }
                 if (indexOfGameObject != 0)
                 {
-                    Undo.RegisterCreatedObjectUndo(obj,"Object");
                     obj.transform.position = new Vector3(-basePosX + j, 1, basePosY - i);
                 }
                 else
                 {
+                    GameObject cube = Instantiate(cubePrefab, this.gameObject.transform);
+                    cube.transform.position = new Vector3(-basePosX + j, 0, basePosY - i);
                     cubeManager.cubesCount += 1;
                 }
             }
+            
         }
-        
+        this.transform.position = new Vector3 (0,1,-0.5f);
+
     }
 }
 
